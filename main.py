1abed58b9e8e07add62aa7e4b50ffbf06aabb54a
avincenthill <alexanderjvincenthill@gmail.com>
2018-03-09 16:53:06 -0800

    add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+#1a for every affiliate_id(Col B), calculate the unique ios_ifa(Col K) & google_aid(Col L) present in the data
+
+
+
+
+#1b calculate how many valid google_aid & ios_ifa exist in the dataset.
+
+
+
+
+#2.1 plot a histogram from the data obtained from exercise 1a
+
+
+
+
+#2.2 plot a histogram of the no. of clicks v/s affiliate_id in the dataset
