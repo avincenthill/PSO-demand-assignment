264adbb86c7c6e274dc084529f506d3b47e2f7e0
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-03-11 20:10:42 -0700

    improve comments

--- main.py
+++ main.py
@@ -33,22 +33,22 @@ numvalidios = len(udf.ios_ifa.value_counts())
 pprint('1b. There are ' + str(numvalidgoogle) + ' valid google_aids in the dataset')
 pprint('1b. There are ' + str(numvalidios) + ' valid ios_ifas in the dataset')
 
-#2.1 -  plot a histogram from the data obtained from exercise 1a
+#2.1 - plot a histogram from the data obtained from exercise 1a
 iddata = pd.DataFrame(columns = ['affiliate_ids', 'numuniqueiosifas', 'numuniquegoogleaids'])
 for i, affiliateid in enumerate(uniqueaffiliateids):
 	numuniqueiosifas = len(df[(df.affiliate_id == affiliateid)].ios_ifa.unique())
 	numuniquegoogleaids = len(df[(df.affiliate_id == affiliateid)].google_aid.unique())
 	iddata.loc[i] = [affiliateid, numuniqueiosifas, numuniquegoogleaids]
 
+#plot 2.1 histogram
 iddata.plot(legend = True, kind = 'bar', x='affiliate_ids', title = '2.1 affiliate_id vs. num_unique_ids')
 
-#2.2 -  plot a histogram of the no. of clicks v/s affiliate_id in the dataset
+#2.2 - plot a histogram of the no. of clicks v/s affiliate_id in the dataset
 clickdata = pd.DataFrame(columns = ['affiliate_ids', 'clicks'])
 for i, affiliateid in enumerate(uniqueaffiliateids):
 	clicks = len(fdf[fdf.affiliate_id == affiliateid])
 	clickdata.loc[i] = [affiliateid, clicks]	
 
+#plot 2.2 histogram
 clickdata.plot(legend = False, kind = 'bar', x='affiliate_ids', y='clicks', title = '2.2 #clicks vs. affiliate_id')
-plt.show() 
-
-#I don't think I'm catching all valid ids as the data seems like compared to Excel insanity checks, but I don't have a lot of time to work on assignments like these. Also the graphs included at the end of the assigment seem unrelated to the pdf problem set, perhaps I'm missing more questions?
+plt.show()
